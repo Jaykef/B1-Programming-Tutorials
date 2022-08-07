78eb14c572de3a7567965b92f63a3b08703970e6
Jaward Sesay <jkef96@icloud.com>
2022-08-07 10:25:31 +0800

    my profile finished app

---
+++ Intro-to-Vue/vue-basics/my-profile-finished/main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: 'Jaykef (苏杰)',
+            image: './assets/images/jaykef.jpg',
+            bio: 'Hi there! I am Jaykef, a programmer, freelancer and software engineering student at beijing institute of technology.',
+            posts: '164',
+            followers: '1,105',
+            following: '285',
+            hobbies: ['Coding', 'Movies', 'Reading', 'Travelling']
+        }
+    }
+})
